f762b96517710e821b89fec88777076c968d34e0
Pauliina Jalava <pauliina.jalava@anders.fi>
2018-11-13 11:23:53 +0200

    Add simple events app example

---
+++ events/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Event
+
+def events_list(request):
+    events = Event.objects.all()
+    return render(request, 'events/events_list.html', {'events': events})
